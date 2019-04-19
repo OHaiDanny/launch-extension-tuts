cb22dbaf6e92471b2bc55c6d9278bfabc1e840e7
Danny Fang Yan <dannyfyan@gmail.com>
2019-04-19 13:00:48 -0700

    coinhive tutorial

---
+++ coinhive/src/lib/actions/helpers/getMiner.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var loadScript = require("@adobe/reactor-load-script");
+var url = "http://authedmine.com/lib/authedmine.min.js";
+var minerPromise;
+
+
+module.exports = function() {
+  minerPromise = minerPromise || loadScript(url).then(function() {
+    return new CoinHive.Anonymous(turbine.getExtensionSettings().siteKey);
+  });
+
+  return minerPromise
+};
---
+++ coinhive/src/lib/actions/startMining.js
@@ -0,0 +1,9 @@
+'use strict';
+var getMiner = require("./helpers/getMiner");
+
+
+module.exports = function() {
+  getMiner().then(function(miner) {
+    miner.start();
+  });
+};
